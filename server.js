e70aa14d93b2428cf3e09e6fdac5a571cc036277
Rob Hess <hessro@oregonstate.edu>
2021-05-25 15:56:26 -0700

    Add code from 5/25 lecture, part 2.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars')
+
 var app = express();
 
 var port = 8000;
+
+app.engine('handlebars', exphbs({ defaultLayout: null }))
+app.set('view engine', 'handlebars')
 
 app.use(express.static('public'));
 
@@ -21,9 +26,15 @@ var availablePeople = [
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
   if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/public/people/' + person + '.html'
-    );
+    // res.status(200).sendFile(
+    //   __dirname + '/public/people/' + person + '.html'
+    // );
+    res.status(200).render('photoPage', {
+      name: "Luke Skywalker",
+      url: "http://placekitten.com/480/480?image=9",
+      caption: "Luke as a kitty (from the server)",
+      displayIfContent: true
+    })
   } else {
     next();
   }
