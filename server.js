7ad7e53036fdad56e04d22c65069f2be8cd1e826
Rob Hess <hessro@oregonstate.edu>
2021-05-27 14:53:56 -0700

    Add code from 5/27 lecture, part 1.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars')
 
+var peopleData = require('./peopleData.json')
+console.log("== peopleData:", peopleData)
+
 var app = express();
 
 var port = 8000;
@@ -15,26 +18,43 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/public/people.html');
 });
 
-var availablePeople = [
-  'luke',
-  'leia',
-  'rey',
-  'finn',
-  'r2d2'
-];
+// var availablePeople = [
+//   'luke',
+//   'leia',
+//   'rey',
+//   'finn',
+//   'r2d2'
+// ];
 
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
-  if (availablePeople.indexOf(person) >= 0) {
+  if (peopleData[person]) {
+    // peopleData["luke"]
+    // peopleData["rey"]
+    res.status(200).render('photoPage', peopleData[person])
     // res.status(200).sendFile(
     //   __dirname + '/public/people/' + person + '.html'
     // );
-    res.status(200).render('photoPage', {
-      name: "Luke Skywalker",
-      url: "http://placekitten.com/480/480?image=9",
-      caption: "Luke as a kitty (from the server)",
-      displayIfContent: true
-    })
+    // res.status(200).render('photoPage', {
+    //   name: "Luke Skywalker",
+    //   // url: "http://placekitten.com/480/480?image=9",
+    //   // caption: "Luke as a kitty (from the server)",
+    //   displayIfContent: false,
+    //   photos: [
+    //     {
+    //       url: "http://placekitten.com/480/480?image=1",
+    //       caption: "Caption #1"
+    //     },
+    //     {
+    //       url: "http://placekitten.com/480/480?image=2",
+    //       caption: "Caption #2"
+    //     },
+    //     {
+    //       url: "http://placekitten.com/480/480?image=3",
+    //       caption: "Caption #3"
+    //     }
+    //   ]
+    // })
   } else {
     next();
   }
